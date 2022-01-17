196b8aa4721ba26f62ea934f34b38bdf5c7665ef
Dominic Mazzoni <dmazzoni@gmail.com>
2022-01-16 21:29:49 -0800

    Improve script so that it makes a bookmarklet directly

--- minify.sh
+++ minify.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
-rm wordle-compact.js
+set -v
+rm -f wordle-compact.js
 cat wordle-script.js | grep -v console.log | grep -v // | grep -v strict > wordle-compact.js
 curl -X POST -s --data-urlencode 'input@wordle-compact.js' https://www.toptal.com/developers/javascript-minifier/raw > wordle.min.js
+echo -n "javascript:" | cat - wordle.min.js | sed 's/ /%20/g' > wordle.bookmarklet.js
+
