754bad002e07341f69f4c9360f0f736705aba86f
Dominic Mazzoni <dmazzoni@gmail.com>
2022-06-26 21:20:32 -0700

    Update index and bookmarklet instructions

--- minify.sh
+++ minify.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -v
-rm -f wordle-compact.js
+rm -f wordle-compact.js wordle.min.js wordle.bookmarklet.js
 cat wordle-script.js | grep -v console.log | grep -v // | grep -v strict > wordle-compact.js
-curl -X POST -s --data-urlencode 'input@wordle-compact.js' https://www.toptal.com/developers/javascript-minifier/raw > wordle.min.js
+curl -X POST -s --data-urlencode 'input@wordle-compact.js' https://www.toptal.com/developers/javascript-minifier/api/raw > wordle.min.js
 echo -n "javascript:" | cat - wordle.min.js | sed 's/ /%20/g' > wordle.bookmarklet.js
 
