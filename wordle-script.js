626e08ba7bb203be16a5b0c3ef1b4cfea78a1460
Dominic Mazzoni <dmazzoni@gmail.com>
2022-06-26 21:05:47 -0700

    Rewrite script based on changes to NYTimes Wordle site

--- wordle-script.js
+++ wordle-script.js
@@ -6,28 +6,35 @@ let setAttribute = (elem, attr, value) => {
     elem.setAttribute(attr, value);
 }
 
-let shadowQuery = (elem, query) => {
-    let root = elem.shadowRoot;
-    if (!root)
-	return null;
-    return root.querySelector(query);
+let myQueryHelper = (elem, query, results) => {
+    if (elem.className.toString().indexOf(query) >= 0) {
+        results.push(elem);
+    }
+    for (let child = elem.firstElementChild; child; child = child.nextElementSibling) {
+        myQueryHelper(child, query, results);
+    }
 };
 
-let shadowQueryAll = (elem, query) => {
-    let root = elem.shadowRoot;
-    if (!root)
-	return [];
-    return root.querySelectorAll(query);
+let myQuery = (elem, query) => {
+    let results = [];
+    myQueryHelper(elem, query, results);
+    return results[0] || null;
+};
+
+let myQueryAll = (elem, query) => {
+    let results = [];
+    myQueryHelper(elem, query, results);
+    return results;
 };
 
 let fix = (elem, makeClickable, role, label) => {
     if (role) {
-	if (role == 'tile') {
+        if (role == 'tile') {
             setAttribute(elem, 'role', 'img');
             setAttribute(elem, 'aria-roledescription', 'tile');
-	} else {
+        } else {
             setAttribute(elem, 'role', role);
-	}
+        }
         if (role == 'dialog') {
             setAttribute(elem, 'aria-modal', false);
         }
@@ -46,12 +53,12 @@ let fix = (elem, makeClickable, role, label) => {
             if (e.code == 'Enter' || e.code == 'Space') {
                 e.stopPropagation();
                 e.preventDefault();
-		let switchChild = shadowQuery(elem, 'div.switch');
-		if (switchChild) {
-		    switchChild.click();
-		} else {
+                let switchChild = myQuery(elem, 'div.switch');
+                if (switchChild) {
+                    switchChild.click();
+                } else {
                     elem.click();
-		}
+                }
             }
         }, false);
     }
@@ -60,36 +67,40 @@ let fix = (elem, makeClickable, role, label) => {
 let masterFixer = (elem) => {
     let newLabel = '';
     ['letter', 'evaluation', 'data-key', 'data-state'].forEach(attr => {
-	let value = elem.getAttribute(attr);
-	if (value) {
-	    newLabel += ' ' + value;
-	}
+        let value = elem.getAttribute(attr);
+        if (value && value != '←' && value != 'tbd') {
+            newLabel += ' ' + value;
+        }
     });
-    if (newLabel != elem.getAttribute('aria-label')) {
-	setAttribute(elem, 'aria-label', newLabel);
+    newLabel = elem.textContent + newLabel;
+    if (newLabel != '' && newLabel != elem.getAttribute('aria-label')) {
+        console.log('Setting ' + elem.getAttribute('role') + ' to ' + newLabel);
+        setAttribute(elem, 'aria-label', newLabel);
     }
 };
 
 let fixAllTiles = (root) => {
-    let tiles = shadowQueryAll(root, 'game-tile');
+    let tiles = myQueryAll(root, 'Tile-module');
     console.log('Tiles: ' + tiles.length);
     for (let j = 0; j < tiles.length; j++) {
-	fix(tiles[j], true, 'tile', 'Tile');
-	masterFixer(tiles[j]);
+        fix(tiles[j], true, 'tile', 'Tile');
+        masterFixer(tiles[j]);
     }
 };
 
 let masterObserver = new MutationObserver((mutationsList, observer) => {
     for (let mutation of mutationsList) {
-	masterFixer(mutation.target);
+        masterFixer(mutation.target);
     }
 });
 
 let watchTile = (tile) => {
     fix(tile, false, 'tile', 'Empty');
-    setAttribute(tile, 'aria-live', 'polite');
     masterFixer(tile);
     masterObserver.observe(tile, { attributes: true });
+    setTimeout(() => {
+        setAttribute(tile, 'aria-live', 'polite');
+    }, 1000);
 }
 
 let watchKey = (key) => {
@@ -98,19 +109,19 @@ let watchKey = (key) => {
 
 let checkboxObserver = new MutationObserver((mutationsList, observer) => {
     for (let mutation of mutationsList) {
-	let checkbox = mutation.target;
-	let newAttr = '' + checkbox.hasAttribute('checked');
-	if (newAttr != checkbox.getAttribute('aria-checked')) {
-	    setAttribute(checkbox, 'aria-checked', newAttr);
-	}
+        let checkbox = mutation.target;
+        let newAttr = '' + checkbox.hasAttribute('checked');
+        if (newAttr != checkbox.getAttribute('aria-checked')) {
+            setAttribute(checkbox, 'aria-checked', newAttr);
+        }
     }
 });
 
 let watchCheckbox = (checkbox) => {
     if (!checkbox.hasAttribute('aria-checked')) {
-	fix(checkbox, true, 'checkbox', checkbox.getAttribute('name'));
-	setAttribute(checkbox, 'aria-checked',
-			      checkbox.hasAttribute('checked'));
+        fix(checkbox, true, 'checkbox', checkbox.getAttribute('name'));
+        setAttribute(checkbox, 'aria-checked',
+                              checkbox.hasAttribute('checked'));
     }
     checkboxObserver.observe(checkbox, { attributes: true });
 }
@@ -119,178 +130,138 @@ let app;
 let previousModalState;
 let firstKey;
 
-let modalObserver = new MutationObserver((mutationsList, observer) => {
-    let modal = mutationsList[0].target;
-    console.log('Modal changed.');
-    console.log(modal);
-    let state = modal.hasAttribute('open');
-    if (previousModalState !== undefined && state != previousModalState) {
-	setTimeout(() => {
-	    if (state) {
-		// Modal is opening, focus inside
-		let stats = shadowQuery(app, 'game-stats');
-		console.log('Modal stats: ' + stats);
-		if (stats) {
-		    let button = shadowQuery(stats, 'button');
-		    console.log('Modal button: ' + button);
-		    if (button) {
-			button.focus();
-		    }
-		}
-	    } else {
-		console.log('First key');
-		if (firstKey)
-		    firstKey.focus();
-	    }
-	}, 1000);
-    }
-    previousModalState = state;
-});
+let fixDialog = (dialog) => {
+    fix(dialog, false, 'dialog', 'Game Modal');
+    setTimeout(() => {
+        console.log('Fixing dialog');
 
-let watchModal = (modal) => {
-    previousModalState = modal.hasAttribute('open');
-    modalObserver.observe(modal,  { attributes: true });
-}
-
-let previousGamePageState;
-
-let gamePageObserver = new MutationObserver((mutationsList, observer) => {
-    let gamePage = mutationsList[0].target;
-    console.log('GamePage changed.');
-    console.log(gamePage);
-    let state = gamePage.hasAttribute('open');
-    if (previousGamePageState !== undefined &&
-	state != previousGamePageState) {
-
-	if (state) {
-	    fix(gamePage, false, 'dialog', 'Game Modal');
+        let close = myQuery(dialog, 'close');
+	if (close) {
+            fix(close, true, 'button', 'Close');
 	}
 
-	setTimeout(() => {
-	    if (state) {
-		let close = shadowQuery(gamePage, 'game-icon[icon=close]');
-		console.log('Close: ' + close);
-		if (close) {
-                    fix(close, true, 'button', 'Close');
-		}
+        // Inside game page could be either settings or help.
+        let gameSettings = myQuery(app, 'Settings-module_setting');
+        if (gameSettings) {
+            gameSettings = gameSettings.parentElement;
+            setAttribute(dialog, 'aria-label', 'Settings Modal');
+            let checkboxes = myQueryAll(gameSettings, 'Settings-module_setting');
+            for (let i = 0; i < checkboxes.length; i++)
+                watchCheckbox(checkboxes[i]);
 
-		// Inside game page could be either settings or help.
-		let gameSettings = shadowQuery(app, 'game-settings');
-		console.log('Game settings: ' + gameSettings);
-		if (gameSettings) {
-		    setAttribute(gamePage, 'aria-label', 'Settings Modal');
-		    let checkboxes = shadowQueryAll(gameSettings, 'game-switch');
-		    console.log('Checkboxes: ' + checkboxes.length);
-		    for (let i = 0; i < checkboxes.length; i++)
-			watchCheckbox(checkboxes[i]);
-
-		    let checkbox = shadowQuery(gameSettings, 'game-switch');
-		    console.log('Checkbox: ' + checkbox);
-		    if (checkbox) {
-			checkbox.focus();
-		    }
-		}
-
-		let help = shadowQuery(app, 'game-help');
-		console.log('Game helps: ' + help);
-		if (help) {
-		    setAttribute(gamePage, 'aria-label', 'Help Modal');
-
-		    fixAllTiles(help);
-		    
-		    close.focus();
-		}
-	    } else {
-		console.log('First key');
-		if (firstKey)
-		    firstKey.focus();
-	    }
-	}, 500);
-    }
-    previousGamePageState = state;
-});
-
-let watchGamePage = (gamePage) => {
-    previousGamePageState = gamePage.hasAttribute('open');
-    gamePageObserver.observe(gamePage,  { attributes: true });
-};
-
-let applyFixes = () => {
-    app = document.querySelector('game-app');
-    console.log('App: ' + app);
-    if (app) {
-        let modal = shadowQuery(app, 'game-modal');
-        console.log('Modal: ' + modal);
-        if (modal) {
-            fix(modal, false, 'dialog', 'Pop-up Modal');
-
-            let close = shadowQuery(modal, 'game-icon[icon=close]');
-            console.log('Close: ' + close);
-            if (close) {
-                fix(close, true, 'button', 'Close');
-                setTimeout(() => {
-                    close.focus();
-                }, 1000);
+            let checkbox = myQuery(gameSettings, 'Settings-module_setting');
+            if (checkbox) {
+                checkbox.focus();
+		return;
             }
         }
 
-	watchModal(modal);
-	
-        let help = shadowQuery(app, 'game-help');
-	console.log('Help: ' + help);
-	if (help) {
-	    fixAllTiles(help);
+        let help = myQuery(app, 'Help-module_instructions');
+        if (help) {
+            setAttribute(dialog, 'aria-label', 'Help Modal');
+
+            fixAllTiles(help);
+        }
+
+        if (close) {
+	    close.focus();
+        }
+
+    }, 500);
+}
+
+let dialogObserver = new MutationObserver((mutationsList, observer) => {
+    let target = mutationsList[0].target;
+
+    setTimeout(() => {
+	let dialog = myQuery(target, 'Page-module_page');
+	if (dialog) {
+	    fixDialog(dialog);
+	    return;
+	}
+	dialog = myQuery(target, 'Modal-module_modalOverlay');
+	if (dialog) {
+	    fixDialog(dialog);
+	    return;
 	}
 
-	let toasters = shadowQueryAll(app, '.toaster');
-	console.log('Toasters: ' + toasters.length);
-	for (let i = 0; i < toasters.length; i++) {
-	    setAttribute(toasters[i], 'aria-live', 'polite');
+        if (firstKey) {
+            firstKey.focus();
+	}
+    }, 500);
+});
+
+let watchForDialogs = (app) => {
+    dialogObserver.observe(app, { childList: true });
+};
+
+let applyFixes = () => {
+    app = myQuery(document.body, 'App-module_game');
+    console.log('App: ' + app);
+    if (app) {
+        let header = myQuery(document.body, 'AppHeader-module_appHeader');
+        if (header) {
+            var buttons = header.querySelectorAll('[tabindex]');
+            buttons.forEach(button => button.setAttribute('tabIndex', 0));
+        }
+
+        watchForDialogs(app);
+
+        let dialog = myQuery(app, 'Modal-module_modalOverlay');
+	if (dialog) {
+	    fixDialog(dialog);
 	}
 
-        let gamePage = shadowQuery(app, 'game-page');
-        console.log('Game page: ' + gamePage);
-	watchGamePage(gamePage);
+        let help = myQuery(app, 'Help-module_page');
+        if (help) {
+            fixAllTiles(help);
+        }
 
-        let keyboard = shadowQuery(app, 'game-keyboard');
+        let toasters = myQueryAll(app, 'ToastContainer-module_toaster');
+        for (let i = 0; i < toasters.length; i++) {
+            setAttribute(toasters[i], 'aria-live', 'polite');
+        }
+
+        let keyboard = myQuery(app, 'Keyboard-module_keyboard');
         console.log('Keyboard: ' + keyboard);
-	fix(keyboard, false, 'group', 'Keyboard');
+        fix(keyboard, false, 'group', 'Keyboard');
 
-	let keys = shadowQueryAll(keyboard, 'button[data-key]');
+        let keys = myQueryAll(keyboard, 'Key-module_key');
         console.log('Keys: ' + keys.length);
-	for (let i = 0; i < keys.length; i++) {
-	    if (!firstKey) {
-		firstKey = keys[i];
-	    }
-	    watchKey(keys[i]);
-	}
+        for (let i = 0; i < keys.length; i++) {
+            if (!firstKey) {
+                firstKey = keys[i];
+            }
+            watchKey(keys[i]);
+        }
 
-        let backspace = shadowQuery(keyboard, 'button[data-key=←]');
-        console.log('Backspace: ' + backspace);
-        fix(backspace, false, null, 'backspace');
+        for (var i = 0; i < keys.length; i++) {
+            if (keys[i].getAttribute('data-key') == '←') {
+                fix(keys[i], false, null, 'backspace');
+            }
+        }
 
-        let rows = shadowQueryAll(app, 'game-row');
+        let rows = myQueryAll(app, 'Row-module_row');
         console.log('Rows: ' + rows.length);
         for (let i = 0; i < rows.length; i++) {
-	    fix(rows[i], false, 'group', 'Row ' + (i + 1));
+            fix(rows[i], false, 'group', 'Row ' + (i + 1));
 
-            let tiles = shadowQueryAll(rows[i], 'game-tile');
-            console.log('Tiles: ' + tiles.length);
+            let tiles = myQueryAll(rows[i], 'Tile-module_tile');
             for (let j = 0; j < tiles.length; j++) {
                 watchTile(tiles[j]);
             }
         }
 
-	let extensionCredit = document.createElement('div');
-	extensionCredit.innerHTML = 'Wordle screen reader accessibility extension running.';
-	rows[rows.length - 1].parentElement.appendChild(extensionCredit);
+        let extensionCredit = document.createElement('p');
+        extensionCredit.style.color = 'var(--color-tone-1)';
+        extensionCredit.innerHTML = 'Wordle screen reader accessibility extension running.';
+        rows[rows.length - 1].parentElement.appendChild(extensionCredit);
     }
 };
 
 setTimeout(() => {
     applyFixes();
 }, 1000);
-
 
 };
 s();
